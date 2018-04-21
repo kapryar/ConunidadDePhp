4d65c95f316a39caa129bdd2e61db0ab2c5425a7
Walter Sánchez <kapry@altopost.com.ar>
2018-04-20 23:53:12 -0300

    Update clase20-4.php

--- clase20-4.php
+++ clase20-4.php
@@ -13,16 +13,34 @@ class OperacionesAritmeticas
        switch($operacion)
        {
          case "suma":
-           $resultado = $datosR[0] + $datosR[1] + $datosR[2]+ $datosR[3]+$datosR[4];
+           //$resultado = $datosR[0] + $datosR[1] + $datosR[2]+ $datosR[3]+$datosR[4];
+           for ($i = 0; $i <$numElementos; $i++ ){
+              $resultado+=  $datosR[$i];
+           }
            break;
          case "resta":
-           $resultado = $datosR[0] - $datosR[1] - $datosR[2] - $datosR[3];
+           //$resultado = $datosR[0] - $datosR[1] - $datosR[2] - $datosR[3];
+           for ($i = 0; $i <$numElementos; $i++ ){
+            $resultado-=  $datosR[$i];
+         }
            break;
          case "multiplica":
-            $resultado = $datosR[0] * $datosR[1] * $datosR[2] * $datosR[3];
+            //$resultado = $datosR[0] * $datosR[1] * $datosR[2] * $datosR[3];
+            $resultado = $datosR[0];
+            for ($i = 0; $i <$numElementos; $i++ ){
+             $resultado = $resultado * $datosR[$i];
+          }
             break;
          case "divide":
-           $resultado = $datosR[0] / $datosR[1] / $datosR[2] / $datosR[3];
+          // $resultado = $datosR[0] / $datosR[1] / $datosR[2] / $datosR[3];
+          $resultado = $datosR[0];
+          for ($i = 0; $i <$numElementos; $i++ ){
+            if($datosR[$i]<>0){
+              $resultado = $resultado / $datosR[$i];
+            }else{
+              $resultado = ' La operacion divide no se permite dividir por 0';
+            }
+          }
            break;
           default:
           $resultado = 'La operacion '.$operacion.' no reconocida';
@@ -31,9 +49,10 @@ class OperacionesAritmeticas
         return $resultado;
       }    
   }
-$datos = array(1,2,3,4,5,6,7);
+//$datos = array(1,2,3,4,5,6,7);
+$datos = array(1,2,0,4,5);
 $objeto = new  OperacionesAritmeticas();
 //$objeto->arreglo();
-echo "El resultado es: ".$objeto->operaciones($datos,"suma");  
+echo "El resultado es: ".$objeto->operaciones($datos,"divide");  
 
 
