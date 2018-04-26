4905cae9e19384055f113d56ca3383fa853f2936
Walter Sánchez <kapry@altopost.com.ar>
2018-04-25 23:19:51 -0300

    nuevo

---
+++ clase25-04.php
@@ -0,0 +1,17 @@
+<?php
+
+class OperacionesAritmeticas
+  {
+     public function __construct()
+    {
+        echo "Metodo magico constructor";
+    }
+
+     public function operaciones($datosR, $operacion="divide")
+      {
+        
+      }    
+  }
+$objeto = new  OperacionesAritmeticas();
+$objeto2 = new  OperacionesAritmeticas();
+
