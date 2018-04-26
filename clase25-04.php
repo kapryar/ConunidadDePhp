499f226048644206eab34ed362f1eb80b3a8fb5c
Walter Sánchez <kapry@altopost.com.ar>
2018-04-26 00:03:14 -0300

    metodos magicos

--- clase25-04.php
+++ clase25-04.php
@@ -4,11 +4,32 @@ class OperacionesAritmeticas
   {
      public function __construct($valor)
     {
-        echo "Metodo magico constructor $valor <br>";
+        echo "Metodo magico constructor $valor  <br>";
     }
 
     public function __destruct(){
         echo "Adios Objeto.<br>";
+    }
+
+    public function __call($nombreMetodo, $argumento)
+    {
+        echo "Estas llamando el metodo $nombreMetodo <br>";
+        echo "Los argumentos son ".$argumento[0]."<br>";
+        if ($nombreMetodo=="suma"){
+            $resultado = 1+1;
+            echo "El resultado es $resultado <br>";
+        }elseif ($nombreMetodo=="resta"){
+            $resultado = 1-1;
+            echo "El resultado es $resultado <br>"; 
+        }
+    }
+
+    public function __get($nombrePropiedad){
+        echo "Estas intentao extraer la propiedad $nombrePropiedad de manera directa. Debes utiliza el metodo obtenerEdad<br>";
+    }
+
+    public function __set($nombrePropiedad){
+        echo "Asignaste el valor a la propiedad $nombrePropiedad de manera directa. Debes utiliza el metodo obtenerEdad<br>";
     }
 
      public function operaciones($datosR, $operacion="divide")
@@ -17,6 +38,11 @@ class OperacionesAritmeticas
       }    
   }
 $objeto = new  OperacionesAritmeticas("1");
-$objeto2 = new  OperacionesAritmeticas("2");
-$objeto2 = null;
- echo "Mucho codigo antes<br>";
+//$objeto2 = new  OperacionesAritmeticas("2");
+//$objeto2 = null;
+//echo "Mucho codigo antes<br>";
+//$objeto->resta();
+
+$objeto->suma(1,2,3,4,5);
+
+echo "La edad es ".$objeto->edad;
